c99e38491d833ddec78240b7d473783a1b61b3ab
ibarhatov <ibarhatov@ya.ru>
2025-09-06 05:41:49 +0200

    validation added

--- src/main/java/com/ibarkhatov/urlshortener/dto/CreateUrlRequest.java
+++ src/main/java/com/ibarkhatov/urlshortener/dto/CreateUrlRequest.java
@@ -1,9 +1,10 @@
 package com.ibarkhatov.urlshortener.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record CreateUrlRequest(
-        @NotBlank String originalUrl
+        @NotBlank @URL String originalUrl
 ) {
 }
 
